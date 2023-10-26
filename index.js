1245dfcc627048dfcc3920bf95623e9d25e8f04c
Hima Nandini Nagalla <95603255+NandiniN01@users.noreply.github.com>
2023-10-26 10:12:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+function callb(){
+			let text= "Can we reserve this table for you?";
+			if(confirm(text)==true){
+				alert("Your table is Reserved");
+			}
+			else{
+				alert("Choose a convenient table")
+			}
+
+		}
+function calld(){
+			let text= "Can we add this item to the list of food items you ordered?";
+			if(confirm(text)==true){
+				alert("Item added to the cart");
+			}
+			else{
+				alert("select the item from the menu")
+			}
+
+		}
+function callm(){
+			let text= "Can we add this item to the list of food items you ordered?";
+			if(confirm(text)==true){
+				alert("Item added to the cart");
+			}
+			else{
+				alert("select the item from the menu")
+			}
+
+		}
